a5e32f13a9286a63eb37a109986111cd25eda20c
LBoonstra <lisa_boonstra@hotmail.com>
2018-09-11 13:35:48 +0200

    updated ex2d and assignmentfile

--- ex02/ex02/ex02d.c
+++ ex02/ex02/ex02d.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-#include <sys/mman.h>
 #include <math.h>
-
+#include <sys/mman.h>
 #include <alchemy/task.h>
 
 RT_TASK demo_task1;
 RT_TASK demo_task2;
 RT_TASK demo_task3;
 
+
+
 void demo(void *arg) {
-	int num = * (int *)arg;
+    RT_TASK_INFO curtaskinfo;
+    rt_task_inquire(NULL,&curtaskinfo);
+	int num = * (int*)arg;
 	rt_task_sleep(1*pow(10,9));
-	rt_task_set_periodic(NULL, TM_NOW,  (RTIME)num*pow(10,9));
-	while (1) {
-		RT_TASK_INFO curtaskinfo;
-		rt_task_inquire(NULL,&curtaskinfo);
-		char str[12];
-		sprintf(str, "%d", num);
-		rt_printf("Task name: %s \n", str);
-		rt_task_wait_period(NULL);
+	rt_task_set_periodic(NULL, TM_NOW, (RTIME) num*pow(10,9));
+	while(1){
+    rt_printf("Task name: %d \n", num);
+	rt_task_wait_period(NULL);
 	}
 	return;
 }
@@ -31,38 +30,41 @@ int main(int argc, char* argv[])
   char  str2[10] ;
   char  str3[10] ;
 
-  // Perform auto-init of rt_print buffers if the task doesn't do so
-  //rt_print_auto_init(1);
 
+  printf("start task\n");
   // Lock memory : avoid memory swapping for this program
   mlockall(MCL_CURRENT|MCL_FUTURE);
+  
+  sprintf(str1,"hello1");
+  sprintf(str2,"hello2");
+  sprintf(str3,"hello3");
 
-  rt_printf("start task\n");
-
-  /*
+  
+  /* Create task
    * Arguments: &task,
    *            name,
    *            stack size (0=default),
    *            priority,
    *            mode (FPU, start suspended, ...)
-   */
-  sprintf(str1,"hello1");
-  sprintf(str2,"hello2");
-  sprintf(str3,"hello3");
+   */  
   rt_task_create(&demo_task1, str1, 0, 50, 0);
   rt_task_create(&demo_task2, str2, 0, 50, 0);
   rt_task_create(&demo_task3, str3, 0, 50, 0);
-    /*
+
+	
+
+  /*  Start task
    * Arguments: &task,
    *            task function,
    *            function argument
    */
-  int indexOne = 1;
-  int indexTwo = 2;
-  int indexThree = 3;
-  rt_task_start(&demo_task1, &demo, &indexOne);
-  rt_task_start(&demo_task2, &demo, &indexTwo);
-  rt_task_start(&demo_task3, &demo, &indexThree);
+
+  int num[3] = {1,2,3};
+  rt_task_start(&demo_task1, &demo, &num[0]);
+  rt_task_start(&demo_task2, &demo, &num[1]);
+  rt_task_start(&demo_task3, &demo, &num[2]);
+
+  
   printf("end program by CTRL-C\n");
   pause();
 }
