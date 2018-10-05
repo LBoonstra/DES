8cac4b4a9e5603f8cc22dd1a60480d29386a8a65
Richard Bisschops <Richard.jh.Bisschops@live.nl>
2018-10-05 13:15:30 +0200

    09a finished, 09b 'done' (what is exactly?)

--- ex09/ex09a.c
+++ ex09/ex09a.c
@@ -6,61 +6,84 @@
 #include <rtdm/gpio.h>
 #include <math.h>
 
+int amount_samples=1000;
+
 RT_TASK timer_task;
-unsigned long long time_samples[10000];
-unsigned long long time_differences[9999];
+unsigned long long time_samples_falling[1000];
+unsigned long long time_samples_rising[1000];
+unsigned long long time_differences_rotation[1000-1];
+unsigned long long time_differences_sensor[1000];
 
 void calc_time_diffs() {
 	unsigned int n;
-	for (n=1; n<10000;n++) {
-		time_differences[n-1]=time_samples[n]-time_samples[n-1];
+	for (n=1; n<amount_samples;n++) {
+		time_differences_rotation[n-1]=time_samples_falling[n]-time_samples_falling[n-1];
+	}
+	for (n=0;n<amount_samples;n++) {
+		time_differences_sensor[n]=time_samples_rising[n]-time_samples_falling[n];
 	}
 }
 
-void write_RTIMES(char * filename, unsigned int number_of_values){
+void write_RTIMES(char * filename, char * filename2, unsigned int number_of_values){
     unsigned int n=0;
-    FILE *file;
+    FILE *file, *file2;
     file = fopen(filename,"w");
+	file2 = fopen(filename2,"w");
     while (n<number_of_values-1) {
-        fprintf(file,"%u,%llu\n",n,time_differences[n]);
+        fprintf(file,"%u,%llu\n",n,time_differences_rotation[n]);
         n++;
     } 
-    fclose(file);
+	fclose(file);
+	n=0;
+	while (n<number_of_values) {
+        fprintf(file2,"%u,%llu\n",n,time_differences_sensor[n]);
+        n++;
+    } 
+	fclose(file2);
   }
 
-long long unsigned calc_average_time(int nsamples){
-	unsigned long long average;
+void calc_average_time(int nsamples){
+	unsigned long long average, averagefr, rotationPerMinute;
+	unsigned long long sumfr=0;
 	unsigned long long sum=0;
 	int i;
+	printf("Hi  %llu\n", time_differences_rotation[0]);
 	for (i=0; i< nsamples-1;i++){
-		sum+= time_differences[i];
+		sum+= time_differences_rotation[i];
 	}
-	average = (unsigned long long) (sum / nsamples);
-	return average;
+	average = (unsigned long long) (sum / (nsamples-1));
+	printf("average rotation length  %llu\n", average);
+	rotationPerMinute = (unsigned long long) 6*pow(10,10) / average;
+	printf("Rotations per minute  %llu\n", rotationPerMinute);
+	for (i=0; i< nsamples;i++){
+		sumfr+= time_differences_sensor[i];
+	}
+	averagefr = (unsigned long long) (sumfr / (nsamples));
+	printf("average sensor  %llu\n", averagefr);
 }
 
 void measureTime(void *arg) {
-	unsigned int nsamples=10000;
+	unsigned int nsamples=amount_samples;
 	int ret, value;
 	int fdr13 = open("/dev/rtdm/pinctrl-bcm2835/gpio23",O_RDONLY);
-	int xeno_trigger=GPIO_TRIGGER_EDGE_FALLING;
+	int xeno_trigger=GPIO_TRIGGER_EDGE_FALLING|GPIO_TRIGGER_EDGE_RISING;
 	ret=ioctl(fdr13, GPIO_RTIOC_IRQEN, &xeno_trigger);
 	int n;
-	while(1) {
+	for (n=0; n<amount_samples;n++) {
 		ret = read(fdr13, &value, sizeof(value));
-		printf("Value:%d\n", value );
-		//time_samples[n]= (int) rt_timer_read();
+		time_samples_falling[n]= (unsigned long long) rt_timer_read();
+		ret = read(fdr13, &value, sizeof(value));
+		time_samples_rising[n]= (unsigned long long) rt_timer_read();
+		
 	}
 	calc_time_diffs();
-	write_RTIMES("time_diff.csv",nsamples);
-	unsigned long long average;
-    average=calc_average_time(nsamples);
-    printf("average  %llu\n", average);
+	write_RTIMES("time_diff_rotation.csv", "time_diff_sensor.csv",nsamples);
+    calc_average_time(nsamples);
 }
 
 int main(int argc, char* argv[])
 {
-  unsigned int nsamples=10000;
+  unsigned int nsamples=amount_samples;
   
 	RTIME *time_diff;
 	time_diff = calloc(nsamples, sizeof(RTIME));
